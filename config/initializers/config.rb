aa44b16de9c76eca21c06aa9b2291915d2239864
Geoff Crittenden <geoffcritt@hotmail.com>
2015-09-24 20:39:11 -0500

    setup figgy

---
+++ config/initializers/config.rb
@@ -0,0 +1,16 @@
+Football::CONFIG = Figgy.build do |config|
+  config.root = Rails.root.join('config')
+  config.add_root Rails.root.join('config/etc/local')
+  config.add_root Rails.root.join('config/etc')
+
+  # config.foo is read from etc/foo.yml
+  config.define_overlay :default, nil
+
+  # config.foo is then updated with values from etc/production/foo.yml
+  config.define_overlay(:environment) { Rails.env }
+
+  # Maybe you need to load XML files?
+  config.define_handler 'xml' do |contents|
+    Hash.from_xml(contents)
+  end
+end
