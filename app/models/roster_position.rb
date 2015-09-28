19e844f00720f1e6e476af6c5b7d8907c3c6839d
Geoff Crittenden <geoffcritt@hotmail.com>
2015-09-28 18:48:05 -0500

    created models

---
+++ app/models/roster_position.rb
@@ -0,0 +1,2 @@
+class RosterPosition < ActiveRecord::Base
+end
---
+++ test/models/roster_position_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class RosterPositionTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
