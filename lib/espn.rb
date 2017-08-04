8e48ce4f69ff8f9704360b83719e801e9eac565a
Geoff Crittenden <geoffcritt@hotmail.com>
2017-08-04 13:28:59 -0500

    fixed season_id in espn module methods

--- lib/espn.rb
+++ lib/espn.rb
@@ -1,19 +1,19 @@
 module Espn
   class << self
     def standings_url
-      "#{BASE_URL}/standings?leagueId=#{LEAGUE_ID}&seasonId=#{season_id}"
+      "#{BASE_URL}/standings?leagueId=#{LEAGUE_ID}&seasonId=#{Season.season_id}"
     end
 
     def scoreboard_url
-      "#{BASE_URL}/scoreboard?leagueId=#{LEAGUE_ID}&seasonId=#{season_id}"
+      "#{BASE_URL}/scoreboard?leagueId=#{LEAGUE_ID}&seasonId=#{Season.season_id}"
     end
 
     def roster_url
-      "#{BASE_URL}/clubhouse?leagueId=#{LEAGUE_ID}&teamId={1}&seasonId=#{season_id}"
+      "#{BASE_URL}/clubhouse?leagueId=#{LEAGUE_ID}&teamId={1}&seasonId=#{Season.season_id}"
     end
 
     def fa_url
-      "#{BASE_URL}/freeagency?leagueId=#{LEAGUE_ID}&seasonId=#{season_id}"
+      "#{BASE_URL}/freeagency?leagueId=#{LEAGUE_ID}&seasonId=#{Season.season_id}"
     end
 
     def scoring_url
